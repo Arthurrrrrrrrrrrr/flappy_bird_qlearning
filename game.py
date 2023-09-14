30affa6aab39a9c4de9335cd627fc058bc1cbbce
Arthurrrrrrrrrrrr <nuvoloni.arthur@gmail.com>
2023-09-14 13:32:24 +0200

    Game synchronization +fps

--- game.py
+++ game.py
@@ -1,97 +1,65 @@
 import pygame
 import sys
 import random
+import numpy as np
 
 pygame.init()
 
 class GameAI:
 
-    def __init__(self):
+    def __init__(self, fps):
+        self.fps = fps
         self.clock = pygame.time.Clock()
         self.reset()
 
     def reset(self):
         self.game_active = True
-
-        self.score = 0
         self.frame_iteration = 0
+        self.score = 0
+        self.current_pipe_idx = 0
+        self.current_pipe = []
 
-        self.gravity = 0.25
+        self.gravity = 2
         self.bird_movement = 0
 
-        self.vertical_distance = 0
-        self.vertical_distance_top_pipe = 0
-        self.vertical_distance_bottom_pipe = 0
-        self.horizontal_distance = 0
-        self.vertical_distance_to_bottom = 0
-        self.vertical_distance_to_top = 0
-        self.prev_bird_y = 0
-        self.vertical_speed = 0
-
-        self.screen = pygame.display.set_mode((576, 1024))
+        self.screen = pygame.display.set_mode((288, 512))
 
         self.background = pygame.image.load('assets/background-day.png').convert()
-        self.background = pygame.transform.scale2x(self.background)
 
         self.bird = pygame.image.load('assets/bluebird-midflap.png').convert_alpha()
-        self.bird = pygame.transform.scale2x(self.bird)
-        self.bird_rect = self.bird.get_rect(center=(100, 512))
-
-        self.floor_base = pygame.image.load("assets/base.png").convert()
-        self.floor_base = pygame.transform.scale2x(self.floor_base)
-        self.floor_x_pos = 0
+        self.bird_rect = self.bird.get_rect(center=(50, 256))
 
         self.message = pygame.image.load("assets/message.png").convert_alpha()
-        self.message = pygame.transform.scale2x(self.message)
-        self.game_over_rect = self.message.get_rect(center=(288, 512))
+        self.game_over_rect = self.message.get_rect(center=(144, 256))
 
         self.counter = pygame.image.load("assets/0.png").convert_alpha()
-        self.counter = pygame.transform.scale2x(self.counter)
-        self.counter_rect = self.counter.get_rect(center=(288, 200))
+        self.counter_rect = self.counter.get_rect(center=(144, 60))
 
         self.pipe_surface = pygame.image.load("assets/pipe-green.png")
-        self.pipe_surface = pygame.transform.scale2x(self.pipe_surface)
         self.pipe_list = []
-        self.pipe_height = [400, 600, 800]
-        self.SPAWNPIPE = pygame.USEREVENT
-        pygame.time.set_timer(self.SPAWNPIPE, 1200)
+        self.pipe_height = [200, 250, 300, 350, 400]
 
-    def get_game_state(self):
-        v_d_t_p = abs(self.vertical_distance_top_pipe)/1450
-        if self.vertical_distance_top_pipe < 0:
-            s_v_d_t_p = 0 
-        else:
-            s_v_d_t_p = 1
-        v_d_b_p = abs(self.vertical_distance_bottom_pipe)/850
-        if self.vertical_distance_bottom_pipe < 0:
-            s_v_d_b_p = 0
-        else :
-            s_v_d_b_p = 1
-        h_d = abs(self.horizontal_distance)/650
-        v_s = abs(self.vertical_speed)/650
-        if self.vertical_speed < 0:
-            s_v_s = 0
-        else:
-            s_v_s = 1
-
-        return v_d_t_p, s_v_d_t_p, v_d_b_p, s_v_d_b_p, h_d, v_s, s_v_s
-
-    def game_floor(self):
-        self.screen.blit(self.floor_base, (self.floor_x_pos, 900))
-        self.screen.blit(self.floor_base, (self.floor_x_pos + 576, 900))
+    def restart(self):
+        self.bird_rect.center = (50, 256)
+        self.bird_movement = 0
+        self.pipe_list.clear()
+        self.game_active = True
+        self.score = 0
+        self.frame_iteration = 0
+        self.current_pipe_idx = 0
 
     def check_collision(self):
         for pipe in self.pipe_list:
             if self.bird_rect.colliderect(pipe):
                 return False
-        if self.bird_rect.top <= -100 or self.bird_rect.bottom >= 900:
+        if self.bird_rect.top <= -50 or self.bird_rect.bottom >= 500:
             return False
         return True
 
     def create_pipe(self):
         random_pipe_pos = random.choice(self.pipe_height)
-        top_pipe = self.pipe_surface.get_rect(midbottom=(700, random_pipe_pos - 300))
-        bottom_pipe = self.pipe_surface.get_rect(midtop=(700, random_pipe_pos))
+        top_pipe = self.pipe_surface.get_rect(bottomleft=(288, random_pipe_pos - 150))
+        bottom_pipe = self.pipe_surface.get_rect(topleft=(288, random_pipe_pos))
         return bottom_pipe, top_pipe
 
     def move_pipes(self):
@@ -101,25 +69,21 @@ class GameAI:
 
     def draw_pipes(self):
         for pipe in self.pipe_list:  
-            if pipe.bottom >= 1024:
+            if pipe.bottom >= 512:
                 self.screen.blit(self.pipe_surface, pipe)
             else:
                 flip_pipe = pygame.transform.flip(self.pipe_surface, False, True)
                 self.screen.blit(flip_pipe, pipe)
 
     def update_counter(self):
-        self.score = len(self.pipe_list) // 2 - 1
         if self.score >= 0 and self.score <= 9:
             counter = pygame.image.load("assets/" + str(self.score) + ".png").convert_alpha()
-            counter = pygame.transform.scale2x(counter)
             return counter
         elif self.score >= 10 and self.score <= 99:
             tens = self.score // 10
             units = self.score % 10
             counter_tens = pygame.image.load("assets/" + str(tens) + ".png").convert_alpha()
             counter_units = pygame.image.load("assets/" + str(units) + ".png").convert_alpha()
-            counter_tens = pygame.transform.scale2x(counter_tens)
-            counter_units = pygame.transform.scale2x(counter_units)
             counter = pygame.Surface((counter_tens.get_width() + counter_units.get_width(), counter_tens.get_height()), pygame.SRCALPHA)
             counter.blit(counter_tens, (0, 0))
             counter.blit(counter_units, (counter_tens.get_width(), 0))
@@ -131,9 +95,6 @@ class GameAI:
             counter_hundreds = pygame.image.load("assets/" + str(hundreds) + ".png").convert_alpha()
             counter_tens = pygame.image.load("assets/" + str(tens) + ".png").convert_alpha()
             counter_units = pygame.image.load("assets/" + str(units) + ".png").convert_alpha()
-            counter_hundreds = pygame.transform.scale2x(counter_hundreds)
-            counter_tens = pygame.transform.scale2x(counter_tens)
-            counter_units = pygame.transform.scale2x(counter_units)
             counter = pygame.Surface((counter_hundreds.get_width() + counter_tens.get_width() + counter_units.get_width(), counter_hundreds.get_height()), pygame.SRCALPHA)
             counter.blit(counter_hundreds, (0, 0))
             counter.blit(counter_tens, (counter_hundreds.get_width(), 0))
@@ -148,10 +109,6 @@ class GameAI:
             counter_hundreds = pygame.image.load("assets/" + str(hundreds) + ".png").convert_alpha()
             counter_tens = pygame.image.load("assets/" + str(tens) + ".png").convert_alpha()
             counter_units = pygame.image.load("assets/" + str(units) + ".png").convert_alpha()
-            counter_thousands = pygame.transform.scale2x(counter_thousands)
-            counter_hundreds = pygame.transform.scale2x(counter_hundreds)
-            counter_tens = pygame.transform.scale2x(counter_tens)
-            counter_units = pygame.transform.scale2x(counter_units)
             counter = pygame.Surface((counter_thousands.get_width() + counter_hundreds.get_width() + counter_tens.get_width() + counter_units.get_width(), counter_thousands.get_height()), pygame.SRCALPHA)
             counter.blit(counter_thousands, (0, 0))
             counter.blit(counter_hundreds, (counter_thousands.get_width(), 0))
@@ -160,34 +117,23 @@ class GameAI:
             return counter
         else:
             counter = pygame.image.load("assets/0.png").convert_alpha()
-            counter = pygame.transform.scale2x(counter)
             return counter
 
     def display_counter(self):
         self.screen.blit(self.counter, self.counter_rect)
 
-    def update_distances_speed(self):
-        # Calculate vertical and horizontal distance to the next pipe
-        if len(self.pipe_list) > 1:
-            next_pipe = self.pipe_list[-1]
-            self.vertical_distance = abs(next_pipe.bottom + 150 - self.bird_rect.centery)
-            self.vertical_distance_top_pipe = next_pipe.top - self.bird_rect.top
-            self.vertical_distance_bottom_pipe = self.bird_rect.bottom - next_pipe.bottom
-            self.horizontal_distance = next_pipe.centerx - self.bird_rect.centerx
-        else:
-            self.vertical_distance = 0
-            self.vertical_distance_top_pipe = 0
-            self.vertical_distance_bottom_pipe = 0
-            self.horizontal_distance = 0
-        # Calculate vertical distance to bottom of the screen
-        self.vertical_distance_to_bottom = 1024 - self.bird_rect.bottom
-        # Calculate vertical distance to top of the screen
-        self.vertical_distance_to_top = self.bird_rect.top
-        # Calculate vertical speed
-        bird_y = self.bird_rect.centery
-        self.vertical_speed = bird_y - self.prev_bird_y
-        # Update the previous vertical position with the current position
-        self.prev_bird_y = bird_y
+    def update_ui(self):
+        pygame.display.update()
+        self.frame_iteration += 1
+        self.clock.tick(self.fps)
+
+    def get_values(self):
+        pipe = self.current_pipe
+        # horizontal distance between the bird and the next pipes
+        horizontal_distance = self.bird_rect.right - pipe.left
+        # vertical distance between the bird and the middle between the next pipes
+        vertical_distance = self.bird_rect.centerx - pipe.top
+        return horizontal_distance, vertical_distance
 
     def play_step_human(self):
 
@@ -198,101 +144,76 @@ class GameAI:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and self.game_active:
                     self.bird_movement = 0
-                    self.bird_movement -= 10 
+                    self.bird_movement -= 15
                 if event.key == pygame.K_SPACE and not self.game_active:
-                    self.bird_rect.center = (100, 512)
-                    self.bird_movement = 0
-                    self.pipe_list.clear()
-                    self.game_active = True
-            if event.type == self.SPAWNPIPE and self.game_active:
-                self.pipe_list.extend(self.create_pipe())
-
+                    self.restart()
+        if self.frame_iteration % 32 == 0 and self.game_active:
+            self.pipe_list.extend(self.create_pipe())
+        if len(self.pipe_list) == 2:
+            self.current_pipe = self.pipe_list[0]
+        if  self.bird_rect.left > self.current_pipe.right :
+            self.score += 1
+            self.current_pipe_idx += 2
+            self.current_pipe = self.pipe_list[self.current_pipe_idx]
+        
         self.screen.blit(self.background, (0, 0))
 
         if self.game_active:
             self.bird_movement += self.gravity
             self.bird_rect.centery += self.bird_movement
             self.screen.blit(self.bird, self.bird_rect)
-
             self.pipe_list = self.move_pipes()
             self.draw_pipes()
-
             self.game_active = self.check_collision()
-
-            self.update_distances_speed()
 
         self.counter = self.update_counter()
         self.display_counter()
 
         if not self.game_active:
             self.screen.blit(self.message, self.game_over_rect)
-
-        self.floor_x_pos -= 1
-        self.game_floor()
-        if self.floor_x_pos <= -576:
-            self.floor_x_pos = 0
     
     def play_step_bot(self, action):
 
-        done = False
-
+        # exit the game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.type == self.SPAWNPIPE and self.game_active:
-                self.pipe_list.extend(self.create_pipe())
-        if action[0] == 1 and self.game_active:
+        # action = jump
+        if action == [1, 0]:
             self.bird_movement = 0
-            self.bird_movement -= 10
-        if not self.game_active: #reset
-            self.bird_rect.center = (100, 512)
-            self.bird_movement = 0
-            self.pipe_list.clear()
-            self.game_active = True
-
+            self.bird_movement -= 15
+        # create new pipes each 32 frames
+        if self.frame_iteration % 32 == 0 and self.game_active:
+            self.pipe_list.extend(self.create_pipe())
+        # initialize current_pipe
+        if len(self.pipe_list) == 2:
+            self.current_pipe = self.pipe_list[0]
+        # increment score and current_pipe when the bird pass the pipes
+        if  self.bird_rect.left > self.current_pipe.right :
+            self.score += 1
+            self.current_pipe_idx += 2
+            self.current_pipe = self.pipe_list[self.current_pipe_idx]
+        # blit background
         self.screen.blit(self.background, (0, 0))
-
+        # apply gravity, move bird, blit bird, move pipes, draw pipes, check collisions
         if self.game_active:
             self.bird_movement += self.gravity
             self.bird_rect.centery += self.bird_movement
             self.screen.blit(self.bird, self.bird_rect)
-
             self.pipe_list = self.move_pipes()
             self.draw_pipes()
-
             self.game_active = self.check_collision()
-
-            self.update_distances_speed()
-
+        # update and display counter
         self.counter = self.update_counter()
         self.display_counter()
-
+        # return True if game over, False if not
         if not self.game_active:
-            self.screen.blit(self.message, self.game_over_rect)
-            done = True
+            return True
+        return False
 
-        self.floor_x_pos -= 1
-        self.game_floor()
-        if self.floor_x_pos <= -576:
-            self.floor_x_pos = 0
-
-        return done
-
-    def update_ui(self):
-        pygame.display.update()
-        self.clock.tick(120)
-
-# game = GameAI()
+# game = GameAI(fps = 30)
 # game.game_active = True
-# max = [0, 0, 0, 0, 0, 0, 0]
-# min = [1000, 1000, 1000, 1000, 1000, 1000, 1000] 
 # while True :
 #     game.play_step_human()
-#     for i in range(len(max)):
-#         if max[i] < game.get_game_state()[i]:
-#             max[i] = game.get_game_state()[i]
-#         if min[i] > game.get_game_state()[i]:
-#             min[i] = game.get_game_state()[i]
-#     print(max, min)
 #     game.update_ui()
